7cefac3d843016d3d1d0c451d7837f9d3fed2d72
Marwan K <marwan.kaouachi@gmail.com>
2022-02-18 09:37:56 +0100

    menu debut

--- Program.cs
+++ Program.cs
@@ -13,6 +13,11 @@ namespace Projet_CSharp_S2
  |_|  \___/ \__,_|_|  |_| |_| |_|_|  \__,_|\___| |_____\__,_|_| |_|\__, |\__\___/|_| |_|
                                                                    |___/                ");
 
+            int choix_menu;
+
+            Console.WriteLine("\n0: Quitter le programme\n\n1: La fourmi de Langton - 1ère Partie.\n2: La fourmi de Langton V2 - 2ème Partie\n");
+            Console.WriteLine("Que voulez vous faire ?");
+
 
         }
     }
